5dd3c9c88cd03a0e1716d5dda733a2b723de6f50
rikoriko724 <rikoobama150724@gmail.com>
2024-11-13 15:01:53 +0900

    service_account.jsonを内容をapp.pyに追加

--- backend/app.py
+++ backend/app.py
@@ -26,6 +26,19 @@ load_dotenv()
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 genai.configure(api_key=GOOGLE_API_KEY)
 
+type =  os.getenv("TYPE")
+project_id = os.getenv("PROJECT_ID")
+private_key_id = os.getenv("PRIVATE_KEY_ID")
+private_key = os.getenv("PRIVATE_KEY").replace("\\n", "\n")
+client_email = os.getenv("CLIENT_EMAIL")
+client_id = os.getenv("CLIENT_ID")
+auth_uri = os.getenv("AUTH_URI")
+token_uri = os.getenv("TOKEN_URI")
+auth_provider_x509_cert_url = os.getenv("AUTH_PROVIDER_X509_CERT_URL")
+client_x509_cert_url = os.getenv("CLIENT_X509_CERT_URL")
+universe_domain = os.getenv("UNIVERSE_DOMAIN")
+
+
 # 画像処理が可能なモデルを初期化
 gemini_pro = genai.GenerativeModel("gemini-1.5-flash")
 
